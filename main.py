3108a354fb5b169161189813561ed120f5a1a43a
min-vely <81990267+min-vely@users.noreply.github.com>
2022-05-13 18:50:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import numpy as np
 import random
 import sys
 
+from player import Player
+
 # from gamestate import GameState
 # from minimax import Minimax
 # from montecarlotreesearch import MonteCarloTreeSearch
@@ -13,65 +15,12 @@ import sys
 
 
 
-# # Returns a move object based on the users input. Does not check if the move is valid.
-# def get_user_move():
-#     print("Example Move: A2 A4")
-#     move_str = input("Your Move: ")
-#     move_str = move_str.replace(" ", "")
+p1 = Player('P1', mcts.MCTS.get_MCTS_move(board, []))
+p2 = Player('P2', mmab.MMAB.get_MMAB_move(board, []))
+# p2 = Player('P2', lambda game_state: random.choice(game_state.get_valid_moves()))
+players = [p1, p2]
 
-#     try:
-#         xfrom = letter_to_xpos(move_str[0:1])
-#         yfrom = 8 - int(move_str[1:2]) # The board is drawn "upside down", so flip the y coordinate.
-#         xto = letter_to_xpos(move_str[2:3])
-#         yto = 8 - int(move_str[3:4]) # The board is drawn "upside down", so flip the y coordinate.
-#         return ai.Move(xfrom, yfrom, xto, yto, False)
-#     except ValueError:
-#         print("Invalid format. Example: A2 A4")
-#         return get_user_move()
 
-# # Returns a valid move based on the users input.
-# def get_valid_user_move(board):
-#     while True:
-#         move = get_user_move()
-#         valid = False
-#         possible_moves = board.get_possible_moves(pieces.Piece.WHITE)
-#         # No possible moves
-#         if (not possible_moves):
-#             return 0
-
-#         for possible_move in possible_moves:
-#             if (move.equals(possible_move)):
-#                 move.castling_move = possible_move.castling_move
-#                 valid = True
-#                 break
-
-#         if (valid):
-#             break
-#         else:
-#             print("Invalid move.")
-#     return move
-
-# # Converts a letter (A-H) to the x position on the chess board.
-# def letter_to_xpos(letter):
-#     letter = letter.upper()
-#     if letter == 'A':
-#         return 0
-#     if letter == 'B':
-#         return 1
-#     if letter == 'C':
-#         return 2
-#     if letter == 'D':
-#         return 3
-#     if letter == 'E':
-#         return 4
-#     if letter == 'F':
-#         return 5
-#     if letter == 'G':
-#         return 6
-#     if letter == 'H':
-#         return 7
-
-#     raise ValueError("Invalid letter.")
 
 #
 # Entry point.
@@ -79,6 +28,8 @@ import sys
 board = board.Board.new()
 print(board.to_string())
 
+
+i = 0
 while True:
     MCTS_move = mcts.MCTS.get_MCTS_move(board, [])
     if (MCTS_move == 0):
@@ -92,6 +43,10 @@ while True:
     board.perform_move(MCTS_move)
     print("MCTS move: " + MCTS_move.to_string())
     print(board.to_string())
+    
+    player = players[i % 2]
+    
+    i += 1
 
     MMAB_move = mmab.MMAB.get_MMAB_move(board, [])
     if (MMAB_move == 0):
@@ -105,3 +60,7 @@ while True:
     board.perform_move(MMAB_move)
     print("MMAB move: " + MMAB_move.to_string())
     print(board.to_string())
+    
+    player = players[i % 2]
+    
+    i += 1
